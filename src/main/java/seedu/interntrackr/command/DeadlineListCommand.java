53a43a8144dd622fab0897cd98c52de7a4ad140e
Chun Nga Lee <leeeugenia16@gmail.com>
2026-03-30 21:45:25 +0800

    fix: Make DeadlineListCommand more defensive
    DeadlineListCommand assumes that the provided application index is
    valid.
    
    Invalid indexes can cause errors when retrieving applications from the
    list.
    
    Let's validate the application index before accessing the
    ApplicationList and throw an exception for invalid values.
    
    Early validation prevents invalid access and ensures more robust command
    execution.

--- src/main/java/seedu/interntrackr/command/DeadlineListCommand.java
+++ src/main/java/seedu/interntrackr/command/DeadlineListCommand.java
@@ -1,5 +1,7 @@
 package seedu.interntrackr.command;
 
+import java.util.logging.Logger;
+
 import seedu.interntrackr.exception.InternTrackrException;
 import seedu.interntrackr.model.Application;
 import seedu.interntrackr.model.ApplicationList;
@@ -11,6 +13,7 @@ import seedu.interntrackr.ui.Ui;
  * Lists deadlines linked to a specific application.
  */
 public class DeadlineListCommand extends Command {
+    private static final Logger logger = Logger.getLogger(ListCommand.class.getName());
 
     private final int index;
 
@@ -28,19 +31,27 @@ public class DeadlineListCommand extends Command {
      */
     @Override
     public void execute(ApplicationList applications, Ui ui, Storage storage) throws InternTrackrException {
+        assert applications != null : "ApplicationList cannot be null";
+        assert ui != null : "Ui cannot be null";
+
+        if (index < 1 || index > applications.getSize()) {
+            throw new InternTrackrException("Invalid application index.");
+        }
 
         Application app = applications.getApplication(index);
         DeadlineList deadlines = app.getDeadlines();
+
+        assert deadlines != null : "DeadlineList cannot be null";
+        logger.info("Executing DeadlineListCommand. Total deadlines assigned to this application: " + deadlines.getSize());
 
         if (deadlines.getSize() == 0) {
             ui.showMessage("No deadlines found for this application.");
             return;
         }
-
         ui.showMessage("Here are the deadlines for this application:");
-
         for (int i = 0; i < deadlines.getSize(); i++) {
             ui.showMessage((i + 1) + ". " + deadlines.getDeadlines().get(i).toString());
         }
+        logger.fine("Listed " + deadlines.getSize() + " deadlines successfully.");
     }
 }
